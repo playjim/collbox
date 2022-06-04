a45fea89de619d252196d75bd82e8e77d70d4c5f
d.borisov <dim.borisov937@gmail.com>
2022-06-05 00:47:53 +0300

    добавил мониторинг метрик датчика

--- sensor-request-manager/src/main/java/ru/playjim/controller/Controller.java
+++ sensor-request-manager/src/main/java/ru/playjim/controller/Controller.java
@@ -1,6 +1,12 @@
 package ru.playjim.controller;
 
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tags;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,15 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.playjim.entities.SensorDto;
 import ru.playjim.services.Producer;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @Slf4j
-@RequiredArgsConstructor
 public class Controller {
-    private final Producer producer;
+    AtomicInteger n = new AtomicInteger(0);
 
+    private final Producer producer;
+    private final MeterRegistry registry;
+    public Controller(Producer producer, MeterRegistry registry) {
+        this.producer = producer;
+        this.registry = registry;
+    }
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/sensor", produces = "application/text")
     public void addSensorCO2(@RequestBody SensorDto sensor) {
+        registry.gauge("sensor.CO2",n);
+        n.set(sensor.getValue());
         log.info(sensor.toString());
         producer.sendSensor(sensor);
     }
